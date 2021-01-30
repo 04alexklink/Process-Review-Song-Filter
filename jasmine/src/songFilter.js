18a736ddbb19c3681ff8dacfb2351e0b43c1993c
04alexklink <alex_klink123@hotmail.co.uk>
2021-01-30 20:10:41 +0000

    Refactor: Guard clauses turned into private methods to follow SRP principles and reduce number of lines in main function/make it more readable

--- jasmine/src/songFilter.js
+++ jasmine/src/songFilter.js
@@ -1,11 +1,9 @@
 function songFilter(song, lowValue = 40, highValue = 1000) {
-  if(lowValue > highValue) {throw new Error ("lowValue is greater than highValue provided.")}
-  if(!Number.isInteger(lowValue) || !Number.isInteger(highValue)) {
-      throw new Error("Please provide integer threshold values")
-  }
-  if(song.length === 0) {throw new Error("No song was inputted")}
+  _iscorrectOrderOfThresholdsInputted(lowValue, highValue)
+  _isThresholdsIntegerValues(lowValue, highValue);
+  _isSongProvided(song);
   var filteredSong = song.map((soundWave) => {
-    if(!Number.isInteger(soundWave)) {throw new Error("You have not inputted integer values for song frequency")}
+    _isSongFrequenciesIntegers(soundWave);
     if(soundWave < lowValue) {
       return lowValue;
     } 
@@ -16,3 +14,22 @@ function songFilter(song, lowValue = 40, highValue = 1000) {
   });
   return filteredSong;
 };
+
+function _iscorrectOrderOfThresholdsInputted (lowValue, highValue) {
+  if(lowValue > highValue) {
+    throw new Error ("lowValue is greater than highValue provided.")
+  }
+}
+function _isThresholdsIntegerValues(lowValue, highValue) {
+  if(!Number.isInteger(lowValue) || !Number.isInteger(highValue)) {
+    throw new Error("Please provide integer threshold values")
+  }
+}
+function _isSongProvided(song) {
+  if(song.length === 0) {throw new Error("No song was inputted")};
+}
+function _isSongFrequenciesIntegers(soundWave) {
+  if(!Number.isInteger(soundWave)) {
+    throw new Error("You have not inputted integer values for song frequency");
+  }
+}
